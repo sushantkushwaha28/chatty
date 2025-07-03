f16192ac05acdf5820e24f036a79c457c9e16268
Sushant Kushwaha <sushantkushwaha289@gmail.com>
2025-07-03 23:30:33 +0530

    Database setup using mongodb

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-fs.writeFile('output.txt','helloworld',(err)=>{
-    if (err) {
-        console.error('Error writing to file:', err);
-    } else {
-        console.log('File written successfully');
-    }
-})
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = (req, res) => {
+    // Handle user signup logic here
+    res.send('Signup route');
+};
+export const login = (req, res) => {
+    // Handle user login logic here
+    res.send('Login route');
+};
+export const logout = (req, res) => {
+    // Handle user logout logic here
+    res.send('Logout route');
+};
