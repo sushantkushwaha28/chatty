9bb13d0e9625e25e830f98103f6d71be49e3cf38
Sushant Kushwaha <sushantkushwaha289@gmail.com>
2025-07-03 17:39:24 +0530

    fs

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,9 @@
-const express = require("express")
+const fs = require('fs');
 
-// suggest something
+fs.writeFile('output.txt','helloworld',(err)=>{
+    if (err) {
+        console.error('Error writing to file:', err);
+    } else {
+        console.log('File written successfully');
+    }
+})
